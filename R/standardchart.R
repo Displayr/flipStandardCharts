d59d6787496672aec341b984156beb74356d73a7
JustinCCYap <justinyapcc@gmail.com>
2016-12-05 15:01:03 +1100

    DS-934

--- R/standardchart.R
+++ R/standardchart.R
@@ -338,10 +338,10 @@ Chart <-   function(y,
                     legend.font.size = 10,
                     legend.position = "right",
                     legend.ascending = TRUE,
-                    margin.top = 80,
-                    margin.bottom = 80,
-                    margin.left = 80,
-                    margin.right = 80,
+                    margin.top = NULL,
+                    margin.bottom = NULL,
+                    margin.left = NULL,
+                    margin.right = NULL,
                     margin.inner.pad = 0,
                     y.title = "",
                     y.title.font.color = rgb(44, 44, 44, maxColorValue = 255),
@@ -1349,6 +1349,14 @@ Chart <-   function(y,
         }
     }
 
+    # Creating the list this way means that properties are not added if they have NULL values
+    margins <- list()
+    margins$t <- margin.top
+    margins$b <- margin.bottom
+    margins$l <- margin.left
+    margins$r <- margin.right
+    margins$pad <- margin.inner.pad
+
     ## Set plotly layout styles
     p <- plotly::layout(p,
         title = title,
@@ -1464,13 +1472,7 @@ Chart <-   function(y,
             categoryarray = x.categoryarray
         ),
         ## MARGINS
-        margin = list(
-            t = margin.top,
-            b = margin.bottom,
-            l = margin.left,
-            r = margin.right,
-            pad = margin.inner.pad
-        ),
+        margin = margins,
         plot_bgcolor = plotly::toRGB(plot.fill.color, alpha = plot.fill.transparency),
         paper_bgcolor = plotly::toRGB(chart.fill.color, alpha = chart.fill.transparency),
         hovermode = hover.mode,
