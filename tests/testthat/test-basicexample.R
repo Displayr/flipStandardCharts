98b4b638a591dd0c1ed24eea1ae68ac7873f27d8
chschan <chschan@gmail.com>
2017-11-06 13:15:30 +1100

    DS-1610: Tidy test-basicexample.R

--- tests/testthat/test-basicexample.R
+++ tests/testthat/test-basicexample.R
@@ -21,7 +21,8 @@ for (func in charting.funcs)
     for (dat in dat.list)
     {
         # Create name which will appear in the error message if test fails
-        filestem <- paste0(tolower(func), "-", dat)
+        # Filestem should be prefixed by test file name to avoid name conflicts
+        filestem <- paste0("basicexample-", tolower(func), "-", dat)
 
         test_that(filestem, {
 
@@ -30,10 +31,18 @@ for (func in charting.funcs)
 
             # Run command and check outputs
             # First, isolate specific cases which behave differently
-            if (filestem == "radar-random")
-                expect_error(eval(parse(text=cmd)))
-            else if (filestem == "pie-random")
+            # For this example pie chart and radar charts
+            # handle negative values differently from the other chart types
+            if (filestem == "basicexample-pie-random")
                 expect_warning(eval(parse(text=cmd)))
+
+
+            # For snapshotting, 'expect_warning' can be unchanged,
+            # But when widgets are expected to give error, they should
+            # be skipped before entering 'test_that'
+            # (i.e. line 44 moved to line 26, and use 'next' to skip snapshot)
+            else if (filestem == "basicexample-radar-random")
+                expect_error(eval(parse(text=cmd)))
 
             # General case for all other combinations
             else
@@ -43,6 +52,12 @@ for (func in charting.funcs)
             # But should be commented out after everything is set up
             #print(pp)
             #readline(prompt=paste0(filestem, ": press [enter] to continue: "))
+
+            # Create snapshot and compare against reference (in flipChartTests)
+            # If none exists, the snapshot will be accepted as the reference
+            #expect_true(TestWidget(pp, filestem))
+
+
 
         })
     }
