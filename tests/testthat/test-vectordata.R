7f1fd04374a6a0dbc38cdd63134bf90cc4e0c9f0
chschan <chschan@gmail.com>
2017-12-04 11:55:41 +1100

    DS-1708: Fix setting of default colors

--- tests/testthat/test-vectordata.R
+++ tests/testthat/test-vectordata.R
@@ -17,12 +17,16 @@ dated <- structure(rnorm(10), .Names=sprintf("2017-01-%s", 1:10))
 gapdates <- structure(rnorm(10), .Names=sprintf("2017-01-%s", c(11:15, 26:30)))
 single <- c(5)
 double <- c(5, 1)
+tabdata <- structure(c(12.375, 11.75, 10.375, 11.375, 11.625, 7.875, 11.875,
+15.75, 7), class = "table", .Dim = 9L, .Dimnames = structure(list(
+    Age = c("18 to 24", "25 to 29", "30 to 34", "35 to 39", "40 to 44",
+    "45 to 49", "50 to 54", "55 to 64", "65 or more")), .Names = "Age"))
 
 # Create lists of all charting functions, data types and options to use
 # Relevant tests for radar charts in test-radar.R
 charting.funcs <- c("Column", "Bar", "Area", "Line", "Scatter", "LabeledScatter")
 dat.list <- c("unnamed", "named", "missing1", "missing13",
-               "gapped", "dated", "gapdates", "single", "double")
+               "gapped", "dated", "gapdates", "single", "double", "tabdata")
 opts <- c('default' = '',
           'datalabel' = 'data.label.show=TRUE',
           'linearfit' = 'fit.type="Linear", fit.ignore.last=T, fit.line.color="red"',
