1b6617aabc57d2daf9037420d32efc512cff0d63
jakehoare <jake_hoare@hotmail.com>
2017-12-21 08:43:19 +1100

    DS-90 coerce vector to matrix

--- R/stream.R
+++ R/stream.R
@@ -36,6 +36,8 @@ Stream <- function(x,
                    margin.bottom = 30,
                    margin.right = 40)
 {
+    if (is.null(dim(x)) || length(dim(x)) == 1L)
+        x <- as.matrix(x)
     if (!is.matrix(x) && !is.data.frame(x) && !is.array(x))
         stop("Stream graphs should have a tabular input (e.g., a matrix).")
 
