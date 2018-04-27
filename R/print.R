a1b93945673e00141de5951a4449500deb10d752
chschan <chschan@gmail.com>
2018-04-27 15:48:25 +1000

    DS-1936: Fix warnings

--- R/print.R
+++ R/print.R
@@ -1,5 +1,7 @@
 #' Printing \code{StandardChart} objects
 #' @method print StandardChart
+#' @param x Object of class \code{StandardChart}
+#' @param ... Not used
 #' @description All objects of class \code{StandardChart} are
 #'    expected to contain a component called htmlwidget is the
 #'    original htmlwidget (e.g. plotly, leaflet, etc)
