50d89a9cda2e3bc355b775e861088f7e38b2303f
Mathew W. McLean <mathew.w.mclean@gmail.com>
2018-06-14 17:40:05 +1000

    Import onRender; DS-2010

--- R/geographicmap.R
+++ R/geographicmap.R
@@ -313,6 +313,7 @@ GeographicMap <- function(x,
 #' @importFrom sp proj4string spTransform
 #' @importFrom stats as.formula
 #' @importFrom htmltools browsable tagList tags
+#' @importFrom htmlwidgets onRender
 leafletMap <- function(coords, colors, min.value, max.range, color.NA, legend.show,
                        legend.title, mult, decimals, suffix, values.hovertext.format,
                        treat.NA.as.0, n.categories, categories, format.function, map.type,
