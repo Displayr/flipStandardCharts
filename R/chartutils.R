f23358b15b661f87c348780e4c2eb1cb7e71a26f
chschan <chschan@gmail.com>
2018-08-16 17:07:54 +1000

    DS-2103: Always specify date axis range as character

--- R/chartutils.R
+++ R/chartutils.R
@@ -352,17 +352,12 @@ formatLabels <- function(dat, type, label.wrap, label.wrap.nchar, x.format, y.fo
 
 getDateAxisRange <- function(label.dates)
 {
-    tmp.dates <- as.numeric(label.dates) * 1000
-    day.len <- 60 * 60 * 24 * 1000  # length of days in seconds
+    tmp.dates <- as.numeric(label.dates)
     diff <- min(abs(diff(tmp.dates)), na.rm = T)
 
-    # plotly handles different time-scales differently
-    if (diff < day.len)
-        range <- as.character(range(label.dates) + (c(-1, 1) * ceiling(0.5 * diff/1000)))
-    else if (diff < 5 * day.len)
-        range <- range(tmp.dates, na.rm = T) + c(-1.0, 0.1) * day.len
-    else
-        range <- range(tmp.dates, na.rm = T) + c(-0.5, 0.5) * diff
+    # Always return date-ranges as characters since there 
+    # seems to be more problems with using milliseconds since plotly v4.8.0
+    range <- as.character(range(label.dates) + (c(-1, 1) * ceiling(0.5 * diff)))
     range
 }
 
