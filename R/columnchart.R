638f100ba9cd0f4a3085e6db8d7ade9e84a53196
flipDevTools <opensource@displayr.com>
2020-03-10 15:59:57 +1100

    Remove JIRA link [ci skip]
    URL link to a JIRA task was added accidentally in an earlier pull
    request in R/columnchart.R. This commit removes it.

--- R/columnchart.R
+++ R/columnchart.R
@@ -456,7 +456,7 @@ Column <- function(x,
     if (bar.gap < 0.0 || bar.gap >= 1.0)
     {
         warning("Parameter 'bar gap' must be between 0 and 1. ",
-                "Invalid 'bar gap'https://numbers.atlassian.net/browse/DS-2822 set to default value of 0.15.")
+                "Invalid 'bar gap' set to default value of 0.15.")
         bar.gap <- 0.15
     }
 
