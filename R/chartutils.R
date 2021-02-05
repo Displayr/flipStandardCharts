9355910afea25efd9037eff2c78f2982d0ab068a
chschan <chschan@users.noreply.github.com>
2021-02-05 16:55:24 +1100

    DS-3223: Re-allow zoom in plotly charts

--- R/chartutils.R
+++ R/chartutils.R
@@ -745,7 +745,7 @@ setAxis <- function(title, side, axisLabels, titlefont,
         if (is.null(nticks) || nticks > num.maxticks)
             nticks <- num.maxticks
     }
-    return (list(title = title, fixedrange = TRUE,
+    return (list(title = title, fixedrange = FALSE,
                  side = side, type = axis.type,
                  tickfont = tickfont,
                  showline = has.line, linecolor = linecolor,
