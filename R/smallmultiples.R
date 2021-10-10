c1b04e4de89a31eff3fe35961efe77a458c56b16
chschan <chschan@users.noreply.github.com>
2021-10-10 22:09:00 +1100

    Fix automatic value for x.bounds.minimum

--- R/smallmultiples.R
+++ R/smallmultiples.R
@@ -220,20 +220,19 @@ SmallMultiples <- function(x,
         }
         else if (chart.type %in% c("Bar", "Pyramid", "BarMultiColor"))
         {
-            if (is.null(x.bounds.minimum))
-                x.bounds.minimum <- values.min
-            if (is.null(x.bounds.maximum))
+            if (is.null(x.bounds.minimum) || is.null(x.bounds.maximum))
             {
+                tmp.labels <- ""
+                # Guess how long data labels will be
                 if (any(data.label.show) && chart.type %in% c("Bar", "BarMultiColor"))
-                {
-                    # Guess how long data labels will be
                     tmp.labels <- sprintf(paste0("%s%.", 0, "f%s"),
                         data.label.prefix, max(all.values), data.label.suffix)
-                    x.bounds.maximum <- setTicks(values.min, NULL, NULL, data = all.values, 
-                        type = "Bar", labels = tmp.labels, label.font.size = 10)$range[2]
-                }
-                else
-                    x.bounds.maximum <- values.max
+                tmp.range <- setTicks(x.bounds.minimum, x.bounds.maximum, NULL, data = all.values, 
+                        type = "Bar", labels = tmp.labels, label.font.size = 10)$range
+                if (is.null(x.bounds.minimum))
+                    x.bounds.minimum <- tmp.range[1]
+                if (is.null(x.bounds.maximum))
+                    x.bounds.maximum <- tmp.range[2]
             }
         }
         else
