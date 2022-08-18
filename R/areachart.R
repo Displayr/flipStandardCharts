1eebf1e5db1ae29bf45319406714eeb063779142
Mathew W. McLean <mathew.w.mclean@gmail.com>
2022-08-18 11:21:47 +1000

    DS-3886: Cast subsetted QTable array to numeric to please rle()

--- R/areachart.R
+++ R/areachart.R
@@ -205,7 +205,7 @@ Area <- function(x,
     has.gap <- FALSE
     for (i in 1:ncol(chart.matrix))
     {
-        na.seq <- rle(!is.finite(chart.matrix[,i]))
+        na.seq <- rle(!is.finite(as.numeric(chart.matrix[,i])))
         n <- length(na.seq$values)
         if (any(na.seq$values[-c(1,n)]))
             has.gap <- TRUE
